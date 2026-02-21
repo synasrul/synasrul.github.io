51011abed9d15b4e7472384d3158191cd154c580
synasrul <141646786+synasrul@users.noreply.github.com>
2026-02-21 17:20:39 +0700

    duit app build

--- duit/duitapp/vite.config.js
+++ duit/duitapp/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/synasrul.github.io/",
+  base: "/duit/duitapp/",
   plugins: [react(), tailwindcss()],
 })
